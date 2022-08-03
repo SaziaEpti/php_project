3a6e84f4a161068b4267dcf3fb303c537a649d5a
SaziaEpti <saziaepti2@gmail.com>
2022-08-04 00:50:53 +0600

    first commit

---
+++ css/JS/test.js
@@ -0,0 +1,18 @@
+//Loop Using
+
+for(let i=0; i<=10; i++){
+    console.log('Epti');
+}
+
+//if else condition
+
+var num = 10;
+if(num %2==0){
+    console.log('Even');
+}else{
+    console.log('Odd');
+}
+
+//Two number addition ,substruction, division, multiplication\
+
+
